159b05d9956ee7d438def916577dbc9215d35f1d
Hugo Aparecido Gregorio de Almeida <hugoapga626@gmail.com>
2025-05-25 11:35:17 -0300

    Tentando

--- Aula06/B.cpp
+++ Aula06/B.cpp
@@ -6,15 +6,25 @@ using namespace std;
 void primos(vector<bool> &nums)
 {
     int n = nums.size();
-    if (n < 2)
-        return;
-    nums[0] = nums[1] = false;
-    for (int i = 2; i * i < n; ++i)
+
+    // Inicializa todas as posições como true (potencialmente primas)
+    // Exceto 0 e 1, que não são primos.
+    if (n > 0)
+        nums[0] = false;
+    if (n > 1)
+        nums[1] = false;
+
+    // Começa do 2
+    for (int i = 2; i * i < n; i++)
     {
+        // Se nums[i] é true, então i é um número primo
         if (nums[i])
         {
-            for (int j = i * i; j < n; j *= i)
-                nums[i] = false;
+            // Marca todos os múltiplos de i (a partir de i*i) como não primos
+            for (int j = i * i; j < n; j += i) // CORREÇÃO: j += i
+            {
+                nums[j] = false; // CORREÇÃO: Marca nums[j] como false
+            }
         }
     }
 }
@@ -22,25 +32,49 @@ void primos(vector<bool> &nums)
 int quaseprimo(int numero, vector<bool> primos)
 {
     int cont = 0;
-    for (int i = 2; i < primos.size(); ++i)
+    for (int i = 2; i < numero; i++)
     {
         if (primos[i])
         {
-            while (numero % i == 0)
+            if (cont > 2)
+                return false;
+            if (numero % i == 0)
             {
-                int j = j / i;
+                cont++;
+                while (numero % i == 0)
+                {
+                    numero = numero / i;
+                }
             }
         }
     }
+    if (cont == 2)
+        return true;
+    else
+        return false;
 }
 
 int main()
 {
     int numero;
     cin >> numero;
-    vector<bool> primosarray(numero, false);
+    vector<bool> primosarray(numero, true);
 
     primos(primosarray);
+    int qtd_quase_primo = 0;
+    for (int i = 2; i <= numero; i++)
+
+    {
+        bool quase_primo = quaseprimo(i, primosarray);
+        cout << "Número é: " << i << endl;
+        cout << quase_primo << endl;
+        if (quase_primo)
+        {
+            qtd_quase_primo++;
+        }
+    }
+
+    cout << qtd_quase_primo << endl;
 
     return 0;
 }
--- Aula06/algumas_funcoes.cpp
+++ Aula06/algumas_funcoes.cpp
@@ -18,20 +18,33 @@ int mmc(int a, int b)
 void primos(vector<bool> &nums)
 {
     int n = nums.size();
-    if (n < 2)
-        return;
-    nums[0] = nums[1] = false;
-    for (int i = 2; i * i < n; ++i)
+
+    // Inicializa todas as posições como true (potencialmente primas)
+    // Exceto 0 e 1, que não são primos.
+    if (n > 0) nums[0] = false;
+    if (n > 1) nums[1] = false;
+
+    // Começa do 2
+    for (int i = 2; i * i < n; i++)
     {
+        // Se nums[i] é true, então i é um número primo
         if (nums[i])
         {
-            for (int j = i * i; j < n; j *= i)
-                nums[i] = false;
+            // Marca todos os múltiplos de i (a partir de i*i) como não primos
+            for (int j = i * i; j < n; j += i) // CORREÇÃO: j += i
+            {
+                nums[j] = false; // CORREÇÃO: Marca nums[j] como false
+            }
         }
     }
 }
 
 int main()
 {
+    vector<bool> primosArray(10, true);
+    primos(primosArray);
+
+    for (int i = 0; i < 10; i++)
+        cout << primosArray[i] << endl;
     return 0;
 }
